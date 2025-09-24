5e5b238e1c1dd8a2dc72913f2652ae265fdbcd6e
GalaxyEstef <estefaniaguero008@gmail.com>
2025-09-24 15:04:27 -0300

    Add files via upload

---
+++ ecommerce/JavaScript/main.js
@@ -0,0 +1,16 @@
+// Ejemplo de alert al cargar la página
+window.onload = () => {
+  alert("¡Bienvenido a Mi Empresa!");
+};
+
+// Función para login simple
+function login() {
+  let usuario = prompt("Ingrese su usuario:");
+  let contraseña = prompt("Ingrese su contraseña:");
+
+  if (usuario === "admin" && contraseña === "1234") {
+    alert("Login exitoso ✅");
+  } else {
+    alert("Usuario o contraseña incorrectos ❌");
+  }
+}
